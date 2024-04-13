435fd9c1dcffc78c96da6124d2bd7e9e387d30cd
shota <s8152.45@gmail.com>
2024-04-13 13:55:06 +0900

    Add: rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'render/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,6 +8,5 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  root 'render#index'
-  # root "posts#index"
+  root 'static_pages#top'
 end
